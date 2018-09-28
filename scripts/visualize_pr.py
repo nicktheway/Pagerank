c95c38999a4c910a956ae50e720c299a2cd6bff3
nicktheway <nikospa96@hotmail.gr>
2018-09-28 18:37:15 +0300

    better script for visualizing results

--- scripts/visualize_pr.py
+++ scripts/visualize_pr.py
@@ -7,10 +7,12 @@ import matplotlib.ticker as mtick
 #import helper functions
 import helpers as hp
 
-pagerankFilePath = "./results/pageranks/web-Google.data"
-serialPagerankFilePath = "./results/serial/pageranks/web-Google.data"
-logFilePath = "./results/logs/web-Google.txt"
-serialLogFilePath = "./results/serial/logs/web-Google.txt"
+dataset = "web-Stanford"
+
+pagerankFilePath = "./results/pageranks/" + dataset + ".data"
+serialPagerankFilePath = "./results/serial/pageranks/" + dataset + ".data"
+logFilePath = "./results/logs/" + dataset + ".txt"
+serialLogFilePath = "./results/serial/logs/" + dataset + ".txt"
 
 plt.style.use('seaborn-deep')
 
@@ -23,6 +25,7 @@ ax.tick_params(axis='y', color='C2')
 cax1 = ax.plot(iterationTimes, color='C2', label='Parallel (ms)')
 cax11 = ax.plot(SiterationTimes, color='C2', ls='--', label='Serial (ms)')
 ax.set_ylabel('Time (ms)')
+legend = ax.legend(loc=0, shadow=True, bbox_to_anchor=(1,0.8), title="Iteration Time")
 
 ax2 = ax.twinx()
 ax2.semilogy()
@@ -30,21 +33,25 @@ ax2.tick_params(axis='y', colors='C1')
 cax2 = ax2.plot(errorProgression, color='C1', label='Parallel')
 cax21 = ax2.plot(SerrorProgression, color='C1', ls='', marker='*', label='Serial')
 ax2.set_ylabel('convergence delta')
-legend = fig.legend(loc=1, bbox_to_anchor=(0.8,0.8), shadow=True)
-#legend.get_frame().set_facecolor('beige')
-
-ax3 = ax.twinx()
-cax3 = ax3.plot(SiterationTimes / iterationTimes, color='C4')
+legend2 = ax2.legend(loc=0, shadow=True, title="Convergence delta")
 
 plt.xlim([0, iterationTimes.size])
 plt.title('Pagerank calculation\'s iteration times.')
-
 
 ax.grid(True, color='C2', linestyle='--', linewidth=0.2)
 ax2.grid(True, color='C1', linestyle='--', linewidth=0.1)
 
 ax.set_xlabel('Iteration #')
 plt.suptitle(dataPath, fontweight='bold')
+plt.show()
+
+fig2 = plt.figure(2)
+ax3 = fig2.subplots()
+cax3 = ax3.plot(SiterationTimes / iterationTimes, color='C4', label='speedup')
+ax3.set_xlabel('Iteration #')
+plt.xlim([0, iterationTimes.size])
+plt.suptitle(dataPath, fontweight='bold')
+plt.title('Pagerank parallel calculation\'s speedup.')
 plt.show()
 
 prCalcTime = sum(iterationTimes)
@@ -58,13 +65,14 @@ print("Total Time = %0.2fms" % (prCalcTime+loadToCrsTime+makeStochasticTime+colo
 pr = np.fromfile(pagerankFilePath, dtype=float)
 pr_cor = np.fromfile(serialPagerankFilePath, dtype=float)
 
-fig2 = plt.figure(2)
-ax3 = fig2.subplots()
-cax3 = ax3.plot(pr-pr_cor)
+fig4 = plt.figure(3)
+ax4 = fig4.subplots()
+cax4 = ax4.plot(pr-pr_cor)
 
 plt.xlim([0, pr.size])
 plt.ylim(bottom=-1e-30, top=1e-30)
-plt.title(dataPath, fontweight='bold')
+plt.title("Pagerank's vector difference between\nthe two implementations.")
+plt.suptitle(dataPath, fontweight='bold')
 plt.xlabel('index')
 plt.ylabel('pagerank')
 
