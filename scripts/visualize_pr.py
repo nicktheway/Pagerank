ddf89078703c9e1ec40177b383061b2dcab4c08c
nicktheway <nikospa96@hotmail.gr>
2018-09-30 06:26:22 +0300

    wait for diades data

--- scripts/visualize_pr.py
+++ scripts/visualize_pr.py
@@ -7,7 +7,7 @@ import matplotlib.ticker as mtick
 #import helper functions
 import helpers as hp
 
-dataset = "web-Google"
+dataset = "web-Google_diades"
 
 pagerankFilePath = "./results/pageranks/" + dataset + ".data"
 serialPagerankFilePath = "./results/serial/pageranks/" + dataset + ".data"
